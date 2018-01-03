260c4dee2498154728f1ffbbf5dcce9c426d9ebd
Deither Corpuz <dthrcrpz@gmail.com>
2018-01-03 21:42:28 +0800

    now working on modal

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,27 +1,29 @@
 @extends('templates.template')
 @section('content')
-	<div class="home_banner">
-		<div class="overlayer">
-			<div class="container">
-				<div class="table">
-					<div class="content">
-						<p class="title">
-							Lost and Found Management Something for Pangasinan I Forgot the Title
-						</p>
-						<div class="columns">
-							<a href="javascript:;" class="lost">LOST SOMETHING</a>
-						</div>
-						<div class="columns">
-							<a href="javascript:;" class="found">FOUND SOMETHING</a>
-						</div>
+<div class="home_banner">
+	<div class="overlayer">
+		<div class="container">
+			<div class="table">
+				<div class="content">
+					<p class="title">
+						Lost and Found Management Something for Pangasinan I Forgot the Title
+					</p>
+					<div class="columns">
+						<a href="javascript:;" class="lost"><i class="fa fa-frown-o" aria-hidden="true"></i> LOST SOMETHING</a>
+					</div>
+					<div class="columns">
+						<a href="javascript:;" class="found"><i class="fa fa-smile-o" aria-hidden="true"></i> FOUND SOMETHING</a>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-	<div class="how_it_works">
-		<div class="container">
-			
-		</div>
+</div>
+<div class="how_it_works">
+	<div class="container">
+		<p class="big"><i class="fa fa-address-book-o" aria-hidden="true"></i></p>
+		<p class="title">About Us</p>
+		<p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 	</div>
+</div>
 @stop
--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -15,8 +15,8 @@
 		<nav>
 			<div class="top">
 				<div class="container">
-					<a href="javascript:;"><i class="fa fa-user" aria-hidden="true"></i> Register</a>
-					<a href="javascript:;"><i class="fa fa-sign-in" aria-hidden="true"></i> Login</a>
+					<a href="javascript:;" id="btnRegister"><i class="fa fa-user" aria-hidden="true"></i> Register</a>
+					<a href="javascript:;" id="btnLogin"><i class="fa fa-sign-in" aria-hidden="true"></i> Login</a>
 				</div>
 			</div>
 			<div class="bottom">
@@ -32,6 +32,60 @@
 		</nav>
 		<div class="content_container">
 			@yield('content')
+		</div>
+		<div class="footer">
+			<div class="container">
+				&copy; Copyright 2018. All Rights Reserved.
+			</div>
+		</div>
+		<div class="mymodal registerModal">
+			<div class="modal">
+				<button class="close"><i class="fa fa-times" aria-hidden="true"></i></button>
+				<div class="header">Register</div>
+				<div class="body">
+					<form method="post" action="/register">
+						<div class="form_group">
+							<label>Name:</label>
+							<input type="text" name="name">
+						</div>
+						<div class="column">
+							<div class="form_group">
+								<label>Email:</label>
+								<input type="text" name="email">
+							</div>
+						</div>
+						<div class="column">
+							<div class="form_group">
+								<label>Mobile Number:</label>
+								<input type="text" name="mobile">
+							</div>
+						</div>
+						<div class="form_group">
+							<label>Address:</label>
+							<input type="text" name="address">
+						</div>
+						<div class="form_group">
+							<label>Username:</label>
+							<input type="text" name="username">
+						</div>
+						<div class="column">
+							<div class="form_group">
+								<label>Password:</label>
+								<input type="password" name="password">
+							</div>
+						</div>
+						<div class="column">
+							<div class="form_group">
+								<label>Re-enter Password:</label>
+								<input type="password" name="password2">
+							</div>
+						</div>
+						<div class="form_group">
+							<button type="submit">Register</button>
+						</div>
+					</form>
+				</div>
+			</div>
 		</div>
 		<script type="text/javascript" src="/js/jquery-3.2.1.min.js"></script>
 		<script type="text/javascript" src="/js/owl.carousel.min.js"></script>
