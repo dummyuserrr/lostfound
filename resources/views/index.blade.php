4c9bd0886a227698fa78f4c1ad812d885346bf2b
Deither <dthrcrpz@gmail.com>
2018-01-03 18:46:16 +0800

    now doing the index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,22 @@
 @extends('templates.template')
 @section('content')
-	wew
+	<div class="home_banner">
+		<div class="overlayer">
+			<div class="container">
+				<div class="table">
+					<div class="content">
+						<p class="title">
+							Lost and Found Management System for Pangasinan
+						</p>
+						<div class="columns">
+							<a href="javascript:;" class="lost">LOST SOMETHING</a>
+						</div>
+						<div class="columns">
+							<a href="javascript:;" class="found">FOUND SOMETHING</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 @stop
