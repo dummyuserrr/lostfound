e733dc02bf5e45169ec06030877c325ee3e5921e
Deither <dthrcrpz@gmail.com>
2018-01-04 18:50:09 +0800

    now working on the lost page still

--- resources/views/lost.blade.php
+++ resources/views/lost.blade.php
@@ -1,4 +1,32 @@
 @extends('templates.template')
 @section('content')
-	haha
+<div class="lost_container">
+	<div class="container">
+		<div class="left">
+			<div class="box">
+				sadsa
+			</div>
+		</div>
+		<div class="right">
+			<div class="box">
+				<div class="column">
+					<div class="form_group">
+						<label>Category: <i>*</i></label>
+						<select required name="category">
+							<option selected disabled>Select Category</option>
+							<option>saa</option>
+							<option>saa</option>
+						</select>
+					</div>
+				</div>
+				<div class="column">
+					<div class="form_group">
+						<label>Photo(s): <i>*</i></label>
+						<input required type="file" name="name">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 @stop
