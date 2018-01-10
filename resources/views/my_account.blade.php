e8fa7750be195cdba1450a407515ee207f1b21d7
Deither <dthrcrpz@gmail.com>
2018-01-10 15:32:42 +0800

    will convert myaccount view to form

--- resources/views/my_account.blade.php
+++ resources/views/my_account.blade.php
@@ -19,22 +19,39 @@
 		<div class="right">
 			<div class="box">
 				<div class="userprofile">
-					<img src="{{ $user->image }}" class="image">
+					<div class="form_group">
+						<label for="myaccount_image">
+							<img src="{{ $user->image }}" class="image myaccount_image">
+						</label>
+						<input type="file" name="image" id="myaccount_image" style="display:none">
+					</div>
+					<div class="form_group">
+						<label>Username:</label>
+						<input required type="text" disabled name="name" value="{{ $user->username }}">
+					</div>
+					<div class="form_group">
+						<label>New Password: (Optional. You can leave this blank)</label>
+						<input type="password" name="password">
+					</div>
+					<div class="form_group">
+						<label>Re-enter New Password:</label>
+						<input type="password" name="password2">
+					</div>
 					<div class="form_group">
 						<label>Name:</label>
-						<input required type="text" disabled name="name" value="{{ $user->name }}">
+						<input required type="text" name="name" value="{{ $user->name }}">
 					</div>
 					<div class="form_group">
 						<label>Address:</label>
-						<input required type="text" disabled name="address" value="{{ $user->address }}">
+						<input required type="text" name="address" value="{{ $user->address }}">
 					</div>
 					<div class="form_group">
 						<label>Email:</label>
-						<input required type="text" disabled name="email" value="{{ $user->email }}">
+						<input required type="text" name="email" value="{{ $user->email }}">
 					</div>
 					<div class="form_group">
 						<label>Mobile:</label>
-						<input required type="text" disabled name="mobile" value="{{ $user->mobile }}">
+						<input required type="text" name="mobile" value="{{ $user->mobile }}">
 					</div>
 				</div>
 			</div>
