247dc4fe2342719261ba7b5d3f1e85a76219bf93
Deither <dthrcrpz@gmail.com>
2018-01-26 20:01:49 +0800

    katamadddd

--- resources/views/adminpanel/template.blade.php
+++ resources/views/adminpanel/template.blade.php
@@ -4,19 +4,21 @@
         <title>Bootstrap Example</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script><link rel="stylesheet" type="text/less" href="/adminpanel/css/cms.less">
+        <link rel="stylesheet" type="text/css" href="/adminpanel/bootstrap-3.3.7-dist/css/bootstrap.min.css">
+        <script type="text/javascript" src="/adminpanel/bootstrap-3.3.7-dist/js/jquery.min.js"></script>
+        <script type="text/javascript" src="/adminpanel/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="/adminpanel/css/cms.less">
         <script type="text/javascript" src="/js/less.min.js"></script>
+        <script type="text/javascript" src="/adminpanel/js/cms.js"></script>
     </head>
     <body>
         <nav class="navbar navbar-inverse" style="border-radius: 0;">
             <div class="container-fluid">
                 <div class="navbar-header">
-                    <a class="navbar-brand" href="#">missingZ</a>
+                    <a class="navbar-brand" href="/">missingZ</a>
                 </div>
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+                    <li><a href="javascript:void(0);"><span class="glyphicon glyphicon-log-out"></span> Logout </a></li>
                 </ul>
             </div>
         </nav>
